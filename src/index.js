039a92a0c847080605c0fec730f1104f6d7095a5
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-07-18 19:57:57 +0530

    added apolloclient

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./Routes";
+import { API } from "./backend";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({ uri: API, cache: new InMemoryCache() });
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import Card from "../../components/Card";
 import Base from "../../layout/Base";
 import { FaTimes } from "react-icons/fa";
+import { useQuery } from "@apollo/client";
+import { getUsers } from "./helpers/homeHelper";
 
 const Home = () => {
+  const { loading, error, data } = useQuery(getUsers);
+  console.log(data);
+
   return (
     <Base>
       <div className='container max-w-5xl mx-auto mt-10'>
