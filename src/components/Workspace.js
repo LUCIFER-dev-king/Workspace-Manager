17ab557cf2c59ba21d9117fb4df41c5ca375e26b
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-07-30 22:18:25 +0530

    refracted code

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -3,7 +3,7 @@ import { FaTimes } from "react-icons/fa";
 const Workspace = () => {
   return (
     <div className='flex justify-between'>
-      <h5>Workspaces</h5>
+      <h5>Workspace</h5>
     </div>
   );
 };
